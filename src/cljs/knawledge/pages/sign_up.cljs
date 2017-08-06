77bad952980e6e45b6fb6b0957692ab6f056603d
Lawrence Pan <lawrencefeipan@gmail.com>
2017-08-05 21:12:44 -0400

    sign up page dumb UI

--- src/cljs/knawledge/pages/sign_up.cljs
+++ src/cljs/knawledge/pages/sign_up.cljs
@@ -3,12 +3,15 @@
 ;; -------------------------
 ;; Components
 
-(defn form-input [label]
+(defn form-input [label props]
   [:div.form-group
    [:label label]
-   [:input.form-control]])
+   [:input.form-control props]])
 
 (defn sign-up-page []
-  [:div.container
-   [:form
-    [form-input "Email address"]]])
+  [:div.container.mt-2
+   [:form.px-3
+    [form-input "Email address" {:type "text"}]
+    [form-input "Password" {:type "password"}]
+    [form-input "Password confirmation" {:type "password"}]
+    [:button.btn.btn-primary.mt-1 "Sign up"]]])
